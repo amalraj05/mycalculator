97e012374edd0bfafd8a307c81442d77fd9b021b
amalraj05 <amalraj.007510@gmail.com>
2024-08-16 00:02:12 +0530

    commit

---
+++ lib/calculator_screen.dart
@@ -0,0 +1,227 @@
+
+
+import 'dart:ffi';
+
+import 'package:flutter/material.dart';
+import 'package:mycalculator/button_values.dart';
+
+class CalculatorScreen extends StatefulWidget {
+  const CalculatorScreen({super.key});
+
+  @override
+  State<CalculatorScreen> createState() => _CalculatorScreenState();
+}
+
+class _CalculatorScreenState extends State<CalculatorScreen> {
+String number1 = ""; // 0 - 9
+String operand = ""; // + - * /
+String number2 = ""; // 0 - 9
+
+  @override
+  Widget build(BuildContext context) {
+    final screenSize=MediaQuery.of(context).size;
+    return Scaffold(
+      body: SafeArea(
+        bottom: false,
+        child: Column(
+          children: [
+            Expanded(
+              child: SingleChildScrollView(
+                reverse: true,
+                child: Container(
+                  alignment: Alignment.bottomRight,
+                  
+                  padding: EdgeInsets.all(16),
+                  child: Text(
+                    "$number1$operand$number2".isEmpty?"0"
+                    :"$number1$operand$number2",
+                  style: TextStyle(
+                    fontSize: 48,fontWeight: FontWeight.bold,
+                  ),
+                  textAlign: TextAlign.end,
+                  ),
+                ),
+              ),
+            ),
+
+            // buttonsection
+
+            Container(
+              height: 410,
+              child: Wrap(
+                children: Btn.buttonValues.map(
+                  (value) =>SizedBox(height: 80,
+                    child: SizedBox(
+                      width: value == Btn.n0? screenSize.width / 2 : (screenSize.width / 4),
+                      height: screenSize.height / 5,
+                      child: buildButton(value)
+                      ),
+                  ),
+                    )
+                    .toList(),
+              ),
+            )
+          ],
+        ),
+      ),
+    );
+  }
+
+  Widget buildButton(value){
+    return Padding(
+      padding: const EdgeInsets.all(8),
+      child: Material(
+        color: getBtnColor(value),
+        clipBehavior: Clip.hardEdge,
+        shape: OutlineInputBorder(
+          borderSide: BorderSide(color: Colors.white30),
+          borderRadius: BorderRadius.circular(100),
+        ),
+        child: InkWell(
+          onTap: () => onBtnTap(value),
+          child: Center(
+            child: Text(value,style: TextStyle(fontWeight: FontWeight.bold,fontSize: 25),)),
+        ),
+      ),
+    );
+  }
+  // #######
+
+  void onBtnTap(String value){
+    if(value == Btn.del) {
+      delete();
+      return;
+    }
+
+    if(value ==Btn.clr){
+      clearall();
+      return;
+    }
+
+    if(value ==Btn.per){
+      convertToPercentage();
+      return;
+    }
+
+    if(value == Btn.calculate){
+      calculate();
+      return;
+    }
+
+    appendValue(value);
+  }
+
+  void calculate() {
+    if (number1.isEmpty)return;
+    if (operand.isEmpty)return;
+    if (number2.isEmpty)return;
+
+    final double num1 = double.parse(number1);
+    final double num2 = double.parse(number2);
+
+    var result=0.0;
+    switch (operand) {
+      case Btn.add:
+      result = num1 + num2;
+      break;
+      case Btn.subtract:
+      result = num1 - num2;
+      break;
+      case Btn.multiply:
+      result = num1 * num2;
+      break;
+      case Btn.divide:
+      result = num1 / num2;
+      break;
+      default:
+    }
+
+    setState(() {
+      number1 = "$result";
+
+      if (number1.endsWith(".0")) {
+        number1 = number1.substring(0, number1.length - 2);
+      }
+
+      operand = "";
+      number2 = "";
+    });
+  }
+
+  void convertToPercentage(){
+    if(number1.isNotEmpty&&operand.isNotEmpty&&number2.isNotEmpty){
+
+      calculate();    }
+
+    if(operand.isNotEmpty){
+      return;
+    }
+
+    final number = double.parse(number1);
+    setState(() {
+      number1 ="${(number / 100)}";
+      operand = "";
+      number2 = "";
+    });
+  }
+
+  void clearall(){
+    setState(() {
+      number1 = "";
+      operand = "";
+      number2 = "";
+
+    });
+  }
+
+  void delete(){
+    if(number2.isNotEmpty){
+      number2=number2.substring(0, number2.length -1);
+    }else if(operand.isNotEmpty){
+      operand="";
+    }else if(number1.isNotEmpty){
+      number1=number1.substring(0, number1.length -1);
+    }
+
+    setState(() {});
+  }
+
+
+  void appendValue(String value){
+    if(value!=Btn.dot&&int.tryParse(value)== null) {
+
+      if(operand.isNotEmpty&&number2.isNotEmpty){
+
+        calculate();
+
+      }
+      operand = value;
+    }else if(number1.isEmpty ||operand.isEmpty){
+      if(value==Btn.dot && number1.contains(Btn.dot)) return;
+      if(value==Btn.dot && number1.isEmpty || number1==Btn.n0) {
+        value = "0.";
+      }
+      number1 += value;
+    } else if(number2.isEmpty ||operand.isNotEmpty){
+      if(value==Btn.dot && number2.contains(Btn.dot)) return;
+      if(value==Btn.dot && number2.isEmpty || number2==Btn.n0) {
+        value = "0.";
+      }
+      number2 += value;
+    }
+    setState(() {});
+  }
+
+  // ####### 
+
+  Color getBtnColor(value){
+    return [Btn.del,Btn.clr].contains(value)?Colors.blueGrey:
+        [Btn.per,
+        Btn.multiply,
+        Btn.add,
+        Btn.subtract,
+        Btn.divide,
+        Btn.calculate,
+        ].contains(value)?Colors.orange:Colors.black87;
+  }
+}
